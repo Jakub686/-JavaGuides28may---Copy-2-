92ca41d4797bea6b77f639e812c47e7d6932fbce
Jakub <jakub.bchyla@gmail.com>
2022-12-11 16:59:17 +0100

    addes employee list

--- backend/src/main/java/com/example/JavaGuides28may/controller/EmployeeController.java
+++ backend/src/main/java/com/example/JavaGuides28may/controller/EmployeeController.java
@@ -62,6 +62,7 @@ public class EmployeeController {
         employee.setFirstName(employeeDetails.getFirstName());
         employee.setLastName(employeeDetails.getLastName());
         employee.setEmailId(employeeDetails.getEmailId());
+        employee.setBool(employeeDetails.getBool());
 
         Employee updatedEmployee = employeeRepository.save(employee);
         return ResponseEntity.ok(updatedEmployee);
--- backend/src/main/java/com/example/JavaGuides28may/model/Employee.java
+++ backend/src/main/java/com/example/JavaGuides28may/model/Employee.java
@@ -24,38 +24,58 @@ public class Employee {
     @Column(name = "email_id")
     private String emailId;
 
+    @Column(name = "bool")
+    private Boolean bool;
+
     public Employee() {
 
     }
 
-    public Employee(String firstName, String lastName, String emailId) {
-        super();
+    public Employee(long id, String firstName, String lastName, String emailId, Boolean bool) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.emailId = emailId;
+        this.bool = bool;
     }
+
     public long getId() {
         return id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
+
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public String getEmailId() {
         return emailId;
     }
+
     public void setEmailId(String emailId) {
         this.emailId = emailId;
+    }
+
+    public Boolean getBool() {
+        return bool;
+    }
+
+    public void setBool(Boolean bool) {
+        this.bool = bool;
     }
 }
