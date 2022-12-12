8e60b566bfef353aff56a0ecff64ae46bcc77c19
Jakub <jakub.bchyla@gmail.com>
2022-12-12 06:11:27 +0100

    lastworking

---
+++ frontned/angular-frontend/src/app/router-test/router-test.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-router-test',
+  templateUrl: './router-test.component.html',
+  styleUrls: ['./router-test.component.css']
+})
+export class RouterTestComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+}
